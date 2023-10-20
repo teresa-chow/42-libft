5f315777dcc23e0670d901c694abb18ec7a958e3
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2023-10-20 20:25:01 +0100

    added missing functions, updated Makefile libft.h

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 10:37:00 by tchow-so          #+#    #+#             */
-/*   Updated: 2023/10/20 08:48:51 by tchow-so         ###   ########.fr       */
+/*   Created: 2023/10/20 13:13:36 by tchow-so          #+#    #+#             */
+/*   Updated: 2023/10/20 13:32:05 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum(int c)
+void	ft_putchar_fd(char c, int fd)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
+	write(fd, &c, 1);
 }
-/*
-int	main(void)
-{
-	char	a = 'A';
-	char	b = '1';
-	char	c = '?';
-
-	printf("isalnum(a): %d\n", isalnum(a));
-	printf("ft_isalnum(a): %d\n", ft_isalnum(a));
-	printf("isalnum(b): %d\n", isalnum(b));
-	printf("ft_isalnum(b): %d\n", ft_isalnum(b));
-	printf("isalnum(c): %d\n", isalnum(c));
-	printf("ft_isalnum(c): %d\n", ft_isalnum(c));
-}*/
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,34 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 10:37:00 by tchow-so          #+#    #+#             */
-/*   Updated: 2023/10/20 08:48:51 by tchow-so         ###   ########.fr       */
+/*   Created: 2023/10/20 13:35:23 by tchow-so          #+#    #+#             */
+/*   Updated: 2023/10/20 13:45:30 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum(int c)
+void	ft_putstr_fd(char *s, int fd)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
-}
-/*
-int	main(void)
-{
-	char	a = 'A';
-	char	b = '1';
-	char	c = '?';
+	int	i;
 
-	printf("isalnum(a): %d\n", isalnum(a));
-	printf("ft_isalnum(a): %d\n", ft_isalnum(a));
-	printf("isalnum(b): %d\n", isalnum(b));
-	printf("ft_isalnum(b): %d\n", ft_isalnum(b));
-	printf("isalnum(c): %d\n", isalnum(c));
-	printf("ft_isalnum(c): %d\n", ft_isalnum(c));
-}*/
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
