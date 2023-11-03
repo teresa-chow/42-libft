41f2222d82163ee61c83362d5c8ad27c2f17b315
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2023-11-03 08:34:27 +0000

    added putendl strjoin substr, updated Make libft.h

---
+++ ft_substr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchow-so <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 12:54:16 by tchow-so          #+#    #+#             */
+/*   Updated: 2023/10/30 12:09:40 by tchow-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (s[start + i] && (i < len))
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+/*
+int	main(void)
+{
+	char const	*s = "testeteste";
+	unsigned int	start = 1;
+	size_t	len = 6;
+
+	printf("start index: %d\nsubstring length: %zu\n", start, len );
+	printf("string: %s\nsubstr: %s\n", s, ft_substr(s, start, len));
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:01:06 by tchow-so          #+#    #+#             */
-/*   Updated: 2023/10/20 14:33:37 by tchow-so         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:35:31 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 #endif
